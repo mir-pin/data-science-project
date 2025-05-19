32347f954894e52850da25ac7b23e3e593af496b
Sara <roggiani.sara@gmail.com>
2025-05-19 14:22:29 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -96,4 +96,98 @@ class JournalUploadHandler(UploadHandler):
         store.close()
 
         return True
-    
+
+
+
+class CategoryUploadHandler(UploadHandler):
+    def __init__(self):
+        super().__init__()  
+
+    def pushDataToDb(self, path):
+        with open(path, "r", encoding="utf-8") as f:
+            json_doc = load(f)
+
+        # 2. Lists for my DataFrames 
+        journals = []
+        my_categories = set()
+        cat_and_quartile = []
+        area_rows = set()
+        cat_area_rows = []
+
+        for journal in json_doc:
+
+            for cat in journal.get("categories", []):
+                cat_id = cat.get("id")
+                quartile = cat.get("quartile")
+                my_categories.add(cat_id)
+                cat_and_quartile.append({
+                    "category_name": cat_id,
+                    "quartile": quartile
+                })
+
+                for area in journal.get("areas"):
+                    area_rows.add(area)
+                    cat_area_rows.append({
+                        "category_name": cat_id,
+                        "area_name": area
+                    })
+
+        # Journals dataframe 
+        idx = 0
+        for journal in json_doc:
+            journal_id = "journal-" + str(idx)
+            idx += 1
+            for id_entry in journal.get("identifiers", []):  # For the actual ISSN and EISSN
+                journals.append({
+                    "journal_id": journal_id,
+                    "identifier": id_entry
+                }) 
+                
+        journals_df = DataFrame(journals)
+        internal_ids = []
+        for idx, row in journals_df.iterrows():
+            internal_ids.append("id-" + str(idx))
+        journals_df.insert(0, "internal_ids", Series(internal_ids, dtype="string"))
+
+        # Categories dataframe
+        # Generate a list of internal identifiers for categories (don't know if it is needed)
+        category_internal_id = []
+        categories_df = DataFrame(list(my_categories))
+        for idx, row in categories_df.iterrows():
+            category_internal_id.append("category-" + str(idx))
+        categories_df.insert(0, "category_ids", Series(category_internal_id, dtype="string"))
+        categories_df = categories_df.rename(columns={0: "category_name"})
+
+        # Areas dataframe 
+        areas_df = DataFrame({"id": list(area_rows)})
+        # Generate a list of internal identifiers as a new column
+        area_internal_id = [] 
+        for idx, row in areas_df.iterrows():
+            area_internal_id.append("area-" + str(idx))
+        areas_df.insert(0, "area_ids", Series(area_internal_id, dtype="string"))
+        areas_df = areas_df.rename(columns={"id": "area_name"})
+
+        # PROBLEM !!! Handle the case in which the quartile is empty !!!
+        # Category-quartile dataframe: dont' know if this is gonna work, maybe it's better just a quartile dataframe 
+        cat_quartile_id =[]
+        cat_quartile_df= DataFrame(cat_and_quartile)
+        cat_quartile_df.drop_duplicates(inplace=True) # inplace=True to modify the original dataframe and avoid duplicates
+        for idx, row in cat_quartile_df.iterrows():
+            cat_quartile_id.append("cat-quartile-" + str(idx))
+        cat_quartile_df.insert(0, "internal_ids", Series(cat_quartile_id, dtype="string"))
+
+        # Category-Area dataframe QUI qualcosa non va non so cosa!!
+        cat_area_df = DataFrame(cat_area_rows)
+        cat_area_df.drop_duplicates(inplace=True)
+        cat_area_df = cat_area_df.merge(categories_df, on="category_name")
+        cat_area_df = cat_area_df.merge(areas_df, on="area_name")
+        cat_area_df = cat_area_df[["category_ids", "area_ids"]]
+
+        with connect("rel.db") as con:
+            journals_df.to_sql("JournalIds", con, if_exists="replace", index=False)
+            categories_df.to_sql("Categories", con, if_exists="replace", index=False)
+            areas_df.to_sql("Areas", con, if_exists="replace", index=False)
+            cat_quartile_df.to_sql("CategoriesQuartile", con, if_exists="replace", index=False)
+            cat_area_df.to_sql("CategoriesAreas", con, if_exists="replace", index=False)
+        con.commit()
+
